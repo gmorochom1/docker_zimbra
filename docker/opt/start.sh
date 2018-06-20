92f0702ee5e84f95c9e51975dfb6a37540e92087
Geovanny <gmorochom1@est.ups.edu.ec>
2018-06-20 09:49:39 -0500

    upfile

---
+++ docker/opt/start.sh
@@ -0,0 +1,166 @@
+#!/bin/sh
+# Preparando todas las variables como IP, nombre de host, etc., todas ellas desde el contenedor
+sleep 5
+HOSTNAME=$(hostname -a)
+DOMAIN=$(hostname -d)
+CONTAINERIP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+RANDOMHAM=$(date +%s|sha256sum|base64|head -c 10)
+RANDOMSPAM=$(date +%s|sha256sum|base64|head -c 10)
+RANDOMVIRUS=$(date +%s|sha256sum|base64|head -c 10)
+
+## Intalacion del servidor DNS ##
+echo "Configurando servidor DNS"
+sed "s/-u/-4 -u/g" /etc/default/bind9 > /etc/default/bind9.new
+mv /etc/default/bind9.new /etc/default/bind9
+rm /etc/bind/named.conf.options
+cat <<EOF >>/etc/bind/named.conf.options
+options {
+directory "/var/cache/bind";
+listen-on { $CONTAINERIP; }; # ns1 dirección IP privada: escucha solo en una red privada
+allow-transfer { none; }; # deshabilita transferencias de zona de manera predeterminada
+forwarders {
+8.8.8.8;
+8.8.4.4;
+};
+auth-nxdomain no; # conform to RFC1035
+#listen-on-v6 { any; };
+};
+EOF
+mv /etc/bind/db.domain /etc/bind/db.$DOMAIN
+
+sed -i 's/\$DOMAIN/'$DOMAIN'/g' \
+  /etc/bind/db.$DOMAIN \
+  /etc/bind/named.conf.local
+
+sed -i 's/\$HOSTNAME/'$HOSTNAME'/g' /etc/bind/db.$DOMAIN
+
+sed -i 's/\$CONTAINERIP/'$CONTAINERIP'/g' /etc/bind/db.$DOMAIN
+
+sudo service bind9 restart
+
+##Creamos el archivo de configuración de Zimbra Colaboration ##
+touch /opt/zimbra-install/installZimbraScript
+cat <<EOF >/opt/zimbra-install/installZimbraScript
+AVDOMAIN="$DOMAIN"
+AVUSER="admin@$DOMAIN"
+CREATEADMIN="admin@$DOMAIN"
+CREATEADMINPASS="$PASSWORD"
+CREATEDOMAIN="$DOMAIN"
+DOCREATEADMIN="yes"
+DOCREATEDOMAIN="yes"
+DOTRAINSA="yes"
+EXPANDMENU="no"
+HOSTNAME="$HOSTNAME.$DOMAIN"
+HTTPPORT="8080"
+HTTPPROXY="TRUE"
+HTTPPROXYPORT="80"
+HTTPSPORT="8443"
+HTTPSPROXYPORT="443"
+IMAPPORT="7143"
+IMAPPROXYPORT="143"
+IMAPSSLPORT="7993"
+IMAPSSLPROXYPORT="993"
+INSTALL_WEBAPPS="service zimlet zimbra zimbraAdmin"
+JAVAHOME="/opt/zimbra/common/lib/jvm/java"
+LDAPAMAVISPASS="$PASSWORD"
+LDAPPOSTPASS="$PASSWORD"
+LDAPROOTPASS="$PASSWORD"
+LDAPADMINPASS="$PASSWORD"
+LDAPREPPASS="$PASSWORD"
+LDAPBESSEARCHSET="set"
+LDAPDEFAULTSLOADED="1"
+LDAPHOST="$HOSTNAME.$DOMAIN"
+LDAPPORT="389"
+LDAPREPLICATIONTYPE="master"
+LDAPSERVERID="2"
+MAILBOXDMEMORY="512"
+MAILPROXY="TRUE"
+MODE="https"
+MYSQLMEMORYPERCENT="30"
+POPPORT="7110"
+POPPROXYPORT="110"
+POPSSLPORT="7995"
+POPSSLPROXYPORT="995"
+PROXYMODE="https"
+REMOVE="no"
+RUNARCHIVING="no"
+RUNAV="yes"
+RUNCBPOLICYD="no"
+RUNDKIM="yes"
+RUNSA="yes"
+RUNVMHA="no"
+SERVICEWEBAPP="yes"
+SMTPDEST="admin@$DOMAIN"
+SMTPHOST="$HOSTNAME.$DOMAIN"
+SMTPNOTIFY="yes"
+SMTPSOURCE="admin@$DOMAIN"
+SNMPNOTIFY="yes"
+SNMPTRAPHOST="$HOSTNAME.$DOMAIN"
+SPELLURL="http://$HOSTNAME.$1:7780/aspell.php"
+STARTSERVERS="yes"
+SYSTEMMEMORY="3.8"
+TRAINSAHAM="ham.$RANDOMHAM@$DOMAIN"
+TRAINSASPAM="spam.$RANDOMSPAM@$DOMAIN"
+UIWEBAPPS="yes"
+UPGRADE="yes"
+USEKBSHORTCUTS="TRUE"
+USESPELL="yes"
+VERSIONUPDATECHECKS="TRUE"
+VIRUSQUARANTINE="virus-quarantine.$RANDOMVIRUS@$DOMAIN"
+ZIMBRA_REQ_SECURITY="yes"
+ldap_bes_searcher_password="$PASSWORD"
+ldap_dit_base_dn_config="cn=zimbra"
+ldap_nginx_password="$PASSWORD"
+ldap_url="ldap://$HOSTNAME.$1:389"
+mailboxd_directory="/opt/zimbra/mailboxd"
+mailboxd_keystore="/opt/zimbra/mailboxd/etc/keystore"
+mailboxd_keystore_password="$PASSWORD"
+mailboxd_server="jetty"
+mailboxd_truststore="/opt/zimbra/common/lib/jvm/java/jre/lib/security/cacerts"
+mailboxd_truststore_password="changeit"
+postfix_mail_owner="postfix"
+postfix_setgid_group="postdrop"
+ssl_default_digest="sha256"
+zimbraDNSMasterIP=""
+zimbraDNSTCPUpstream="no"
+zimbraDNSUseTCP="yes"
+zimbraDNSUseUDP="yes"
+zimbraDefaultDomainName="$DOMAIN"
+zimbraFeatureBriefcasesEnabled="Enabled"
+zimbraFeatureTasksEnabled="Enabled"
+zimbraIPMode="ipv4"
+zimbraMailProxy="FALSE"
+zimbraMtaMyNetworks="127.0.0.0/8 $CONTAINERIP/32 [::1]/128 [fe80::]/64"
+zimbraPrefTimeZoneId="America/Guayaquil"
+zimbraReverseProxyLookupTarget="TRUE"
+zimbraVersionCheckInterval="1d"
+zimbraVersionCheckNotificationEmail="admin@$DOMAIN"
+zimbraVersionCheckNotificationEmailFrom="admin@$DOMAIN"
+zimbraVersionCheckSendNotifications="TRUE"
+zimbraWebProxy="FALSE"
+zimbra_ldap_userdn="uid=zimbra,cn=admins,cn=zimbra"
+zimbra_require_interprocess_security="1"
+zimbra_server_hostname="$HOSTNAME.$DOMAIN"
+##Paquetes que vamos a instalar ##
+INSTALL_PACKAGES="zimbra-core zimbra-ldap zimbra-logger zimbra-mta zimbra-snmp zimbra-store zimbra-apache zimbra-spell zimbra-memcached zimbra-proxy"
+EOF
+##Instalacion de Zimbra Collaboration ##
+echo "Descargando Zimbra Collaboration 8.7.11"
+wget -O /opt/zimbra-install/zimbra-zcs-8.7.11.tar.gz https://files.zimbra.com/downloads/8.7.11_GA/zcs-8.7.11_GA_1854.UBUNTU16_64.20170531151956.tgz
+
+echo "Extrayendo archivos del fichero"
+tar xzvf /opt/zimbra-install/zimbra-zcs-8.7.11.tar.gz -C /opt/zimbra-install/
+
+echo "Instalacion solo el software de Zimbra Collaboration"
+cd /opt/zimbra-install/zcs-* && ./install.sh -s < /opt/zimbra-install/llaves
+
+echo "Añadiendo la configuración a la instalación de Zimbra Collaboration"
+/opt/zimbra/libexec/zmsetup.pl -c /opt/zimbra-install/installZimbraScript
+
+if [[ $1 == "-d" ]]; then
+  while true; do sleep 1000; done
+fi
+
+if [[ $1 == "-bash" ]]; then
+  /bin/bash
+fi
